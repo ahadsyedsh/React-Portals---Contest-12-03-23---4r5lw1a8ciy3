4ad87e19e16b3dedb027ccc76574f55e5f248d31
syed ahad <110287582+ahadsyedsh@users.noreply.github.com>
2023-03-12 13:57:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,28 @@ import React ,{useState, useRef} from 'react'
 import '../styles/App.css';
 import PortalButton from './PortalButton.js';
 import PortalTextArea from './PortalTextArea.js';
+import { createPortal } from 'react-dom';
 
 //complete the function 
 const App = () => {
 
-
-
+const[input,setInput]=useState("");
+const[inputValue,setInputValue]=useState(input)
+const inputChange = (e) =>{
+  setInput(e.target.value);
+}
+const buttonClick =()=>{
+  setInputValue(input)
+  setInput('')
+}
   return (
     
     <div id="main">
-      <input id="input" value={} onChange={}></input><br/>
-      <PortalButton buttonclick={} /><br/>
-      <PortalTextArea value={}/>
+      <input id="input" value={input} onChange={inputChange}></input><br/>
+      {createPortal(<PortalButton buttonclick={buttonClick} />,document.getElementById("portal-button"))}
+      {createPortal(   <PortalTextArea value={inputValue}/> ,document.getElementById("portal-textarea"))}
+      {/* <PortalButton buttonclick={buttonClick} /><br/>
+      <PortalTextArea value={inputVlaue}/> */}
     </div>
   )
 }
